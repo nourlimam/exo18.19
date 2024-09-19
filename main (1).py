8c707603eb805fe57ad3a5813ba6a4df6d2a3f1e
Nourlims <limnour18@gmail.com>
2024-09-19 12:10:42 +0200

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,76 @@
+'''ex 8
+def valeur_absolue(x):
+    if abs(-9):
+       return(x)
+print (valeur_absolue(9))'''
+
+
+
+'''ex 9
+def est_entier (x):
+    if x==(int):
+        return True 
+    else:
+        return False
+print(est_entier(2.5))'''
+
+
+
+'''ex 10
+def est_pair(n):
+    if n%2==0:
+        return True
+    else :
+        return False
+print(est_pair(14))'''
+
+
+'''ex 11
+def intervalle1(x):
+    if (x> -2) and (x<3):
+        return True
+    else:
+        return False
+print(intervalle1(2))'''
+
+
+
+
+
+'''ex 12
+def sign(nombre):
+    if nombre > 0:
+        return 'positif'
+    elif nombre ==0:
+        return 'nul'
+    else:
+        return 'negatif'
+print(sign(7))'''
+
+
+'''ex 13
+def est_bissewtile(n):
+      if ((n%4==0 and n%100!=0) or (n%400==0)):
+            return True
+      else:
+            return False
+print(est_bissewtile(2000))'''
+
+
+
+'''ex multi
+def nb_pairs(n):
+  for k in range (n+1):
+    print(100*k)
+nb_pairs(10)'''
+
+
+
+
+def suite(n):
+    u = 1
+    for k in range(n):
+      u = 2*u+k
+    return u
+print(suite(60))
+    
